0ef1045a7de35183432574287d4efd8053690870
Travis024 <travis.torline@colorado.edu>
2020-02-05 10:24:39 -0700

    Created a function to print the board to the screen

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,9 +15,13 @@ No arguments are required to run this program.
 //Creates an empty board for the TicTacToe game to be played on
 void CreateBoard(char game_board[3][3]);
 
+//Prints the board to the screen so players can see it
+void DisplayBoard(char game_board[3][3]);
+
 int main(){
   char game_board[3][3];
   CreateBoard(game_board);
+  DisplayBoard(game_board);
 }
 
 
@@ -34,5 +38,24 @@ void CreateBoard(char game_board[3][3]){
       game_board[i][j] = '-';
     }
   }
+
+}
+
+/*
+Prints the board to the screen so players can see it
+
+@param: The game board being used for the TicTacToe game
+return: Prints the current state of the game board to the screen
+*/
+void DisplayBoard(char game_board[3][3]){
+
+  std::cout << "\n";
+
+  std::cout << " " << game_board[0][0] << " " << "|" << " " << game_board[0][1] << " " << "|" << " " << game_board[0][2] << " " << "\n";
+	std::cout << "---|---|---" << "\n";
+	std::cout << " " << game_board[1][0] << " " << "|" << " " << game_board[1][1] << " " << "|" << " " << game_board[1][2] << " " << "\n";
+	std::cout << "---|---|---" << "\n";
+	std::cout << " " << game_board[2][0] << " " << "|" << " " << game_board[2][1] << " " << "|" << " " << game_board[2][2] << " " << "\n";
+
 
 }
