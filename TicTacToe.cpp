8c4d3d656bb35dcbe8be2cd3df314d7d582541c1
Travis024 <travis.torline@colorado.edu>
2020-02-05 16:45:06 -0700

    Added a game loop so the game can actually be played and added comments

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,30 +14,44 @@ No arguments are required to run this program.
 
 //Creates an empty board for the TicTacToe game to be played on
 void CreateBoard(char game_board[3][3]);
+
+//Prints the board to the screen so the players can see it
 void DisplayBoard(char game_board[3][3]);
+
+//Gets the position that a Player would like to put their marker
+std::string GetPlayerChoice();
+
+//Updates the board with a marker in the position requested by the player
 void PlaceMarker(char game_board[3][3], std::string location, char marker);
+
+//Checks to see if there are three in a row anywhere on the board
 bool CheckWin(char game_board[3][3]);
 
-//Prints the board to the screen so players can see it
-
-std::string GetPlayerChoice();
 int main(){
   char game_board[3][3];
   CreateBoard(game_board);
-  DisplayBoard(game_board);
-  std::string test_choice = GetPlayerChoice();
-  PlaceMarker(game_board, test_choice, 'X');
-  DisplayBoard(game_board);
-  test_choice = GetPlayerChoice();
-  PlaceMarker(game_board, test_choice, 'X');
-  DisplayBoard(game_board);
-  test_choice = GetPlayerChoice();
-  PlaceMarker(game_board, test_choice, 'X');
-  DisplayBoard(game_board);
 
-  if(CheckWin(game_board)){
-    std::cout<<"Done!"<<"\n";
+  bool continueGame = false;
+  char marker = 'X';
+  std::string player_choice;
+
+  while(!continueGame){
+    DisplayBoard(game_board);
+    player_choice = GetPlayerChoice();
+    PlaceMarker(game_board, player_choice, marker);
+
+    if(marker == 'X'){
+      marker = 'O';
+    }else{
+      marker = 'X';
+    }
+
+    continueGame = (CheckWin(game_board));
   }
+
+  DisplayBoard(game_board);
+  std::cout<<"Game over! We have a winner!"<<"\n";
+
 }
 
 
@@ -75,15 +89,27 @@ void DisplayBoard(char game_board[3][3]){
 	std::cout << " " << game_board[2][0] << " " << "|" << " " << game_board[2][1] << " " << "|" << " " << game_board[2][2] << " " << "\n";
 }
 
+/*
+Gets the position that a Player would like to position their marker
+
+@param: None
+@return: Returns a string with the poisiton input from the player
+*/
 std::string GetPlayerChoice(){
 
 	std::string string_choice;
-	std::cout << "what is your choice" << std::endl;
+	std::cout << "Enter a position between 1-9 to place your marker: " << std::endl;
 	std::cin >> string_choice;
 
 	return string_choice;
 }
 
+/*
+Gets the position that a Player would like to position their marker
+
+@param: The game board being used for the game, a string specifiying a location, and a character marker to place in that location
+@return: Updates the passed postion with he passed marker
+*/
 void PlaceMarker(char game_board[3][3], std::string location, char marker){
 
   if(location == "1"){
@@ -108,23 +134,30 @@ void PlaceMarker(char game_board[3][3], std::string location, char marker){
 
 }
 
+/*
+Gets the position that a Player would like to position their marker
+
+@param: The game board being used for the game
+@return: Returns true if three in a row is found anywhere on the board, and false if there are no instances of three in a row
+*/
 bool CheckWin(char game_board[3][3]){
 
-  if((game_board[0][0] == game_board[0][1]) && (game_board[0][0] == game_board[0][1])){
+  //Must ensure that the three in a row is NOT the "empty" character, or '-'. These are just spaces without any markers placed yet.
+  if((game_board[0][0] == game_board[0][1]) && (game_board[0][0] == game_board[0][2]) && (game_board[0][0] != '-')){
     return true;
-  }else if((game_board[1][0] == game_board[0][1]) && (game_board[1][1] == game_board[1][2])){
+  }else if((game_board[1][0] == game_board[1][1]) && (game_board[1][0] == game_board[1][2]) && (game_board[1][0] != '-')){
     return true;
-  }else if((game_board[2][0] == game_board[2][1]) && (game_board[2][0] == game_board[2][2])){
+  }else if((game_board[2][0] == game_board[2][1]) && (game_board[2][0] == game_board[2][2]) && (game_board[2][0] != '-')){
     return true;
-  }else if((game_board[0][0] == game_board[1][0]) && (game_board[0][0] == game_board[2][0])){
+  }else if((game_board[0][0] == game_board[1][0]) && (game_board[0][0] == game_board[2][0]) && (game_board[0][0] != '-')){
     return true;
-  }else if((game_board[1][0] == game_board[1][1]) && (game_board[1][0] == game_board[1][2])){
+  }else if((game_board[1][0] == game_board[1][1]) && (game_board[1][0] == game_board[1][2]) && (game_board[1][0] != '-')){
     return true;
-  }else if((game_board[2][0] == game_board[2][1]) && (game_board[2][0] == game_board[2][2])){
+  }else if((game_board[2][0] == game_board[2][1]) && (game_board[2][0] == game_board[2][2]) && (game_board[2][0] != '-')){
     return true;
-  }else if((game_board[0][0] == game_board[1][1]) && (game_board[0][0] == game_board[2][2])){
+  }else if((game_board[0][0] == game_board[1][1]) && (game_board[0][0] == game_board[2][2]) && (game_board[0][0] != '-')){
     return true;
-  }else if((game_board[0][2] == game_board[1][1]) && (game_board[0][2] == game_board[2][0])){
+  }else if((game_board[0][2] == game_board[1][1]) && (game_board[0][2] == game_board[2][0]) && (game_board[0][2] != '-')){
     return true;
   }else{
     return false;
