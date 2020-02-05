5315d5d90ebf9e0b9f1b9a22266ea1d09c75f587
Travis024 <travis.torline@colorado.edu>
2020-02-05 07:58:51 -0700

    Added a CreateBoard function that takes a 3X3 array of char

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TicTacToe
 A TicTacToe game for Object Oriented Programming Assignment PE4
+Type 'make' and then './TicTacToe' to run the game.
+
+## Team Members:
+Travis Torline and Chris Gilbert
---
+++ TicTacToe.cpp
@@ -0,0 +1,38 @@
+/*
+Travis Torline and Chris Gilbert
+
+This program creates a simple two-player game to Tic-Tac-Toe
+Users are asked to input the desired location of their mark
+A winner is determined by who gets three in a row
+
+No arguments are required to run this program.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+//Creates an empty board for the TicTacToe game to be played on
+void CreateBoard(char game_board[3][3]);
+
+int main(){
+  char game_board[3][3];
+  CreateBoard(game_board);
+}
+
+
+/*
+Creates an empty board for the TicTacToe game to be played on
+
+@param: A 3x3 array of characters to be used for the TicTacToe game
+@return: The array will be filled with dashes ('-') to indicate empty spaces.
+*/
+void CreateBoard(char game_board[3][3]){
+
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      game_board[i][j] = '-';
+    }
+  }
+
+}
